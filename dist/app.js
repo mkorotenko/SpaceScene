05d7fbc3759d766139f90498b1341c0739de6e75
Maxim Korotenko <m.v.korotenko@gmail.com>
2018-05-27 01:41:00 +0300

    added planet

--- dist/app.js
+++ dist/app.js
@@ -4,14 +4,23 @@ var mouseX = 0, mouseY = 0;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 var stats = new Stats();
+var controls;
+var clock = new THREE.Clock();
+var radius = 6371;
+var tilt = 0.41;
+var cloudsScale = 1.005;
+var moonScale = 0.23;
+var rotationSpeed = 0.02;
+var meshPlanet, meshClouds, meshMoon;
 
 init();
 animate();
 function init() {
+
     container = document.createElement( 'div' );
     document.body.appendChild( container );
 
-    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.05, 1e7 );
     camera.position.z = 20;
 
     // scene
@@ -23,6 +32,7 @@ function init() {
     var pointLight = new THREE.PointLight( 0xffffff, 0.8 );
     camera.add( pointLight );
     scene.add( camera );
+
     // model
     var onProgress = function ( xhr ) {
         if ( xhr.lengthComputable ) {
@@ -31,7 +41,6 @@ function init() {
         }
     };
     var onError = function ( xhr ) { };
-    //THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader() );
     new THREE.MTLLoader()
         .setPath( 'models/obj/VoyagerNCC74656/' )
         .load( 'voyager.mtl', function ( materials ) {
@@ -46,19 +55,58 @@ function init() {
                 });
         } );
     //
+    var textureLoader = new THREE.TextureLoader();
+    // planet
+    var materialNormalMap = new THREE.MeshPhongMaterial( {
+        specular: 0x333333,
+        shininess: 15,
+        map: textureLoader.load( "textures/planets/earth_atmos_2048.jpg" ),
+        specularMap: textureLoader.load( "textures/planets/earth_specular_2048.jpg" ),
+        normalMap: textureLoader.load( "textures/planets/earth_normal_2048.jpg" ),
+        normalScale: new THREE.Vector2( 0.85, 0.85 )
+    } );
+    geometry = new THREE.SphereBufferGeometry(radius, 100, 50);
+    meshPlanet = new THREE.Mesh(geometry, materialNormalMap);
+    meshPlanet.position.x = 7000;
+    meshPlanet.rotation.y = 0;
+    meshPlanet.rotation.z = tilt;
+    scene.add(meshPlanet);
+    // clouds
+    var materialClouds = new THREE.MeshLambertMaterial({
+        map: textureLoader.load("textures/planets/earth_clouds_1024.png"),
+        transparent: true
+    });
+    meshClouds = new THREE.Mesh( geometry, materialClouds );
+    meshClouds.scale.set( cloudsScale, cloudsScale, cloudsScale );
+    meshClouds.rotation.z = tilt;
+    scene.add( meshClouds );
+    // moon
+    var materialMoon = new THREE.MeshPhongMaterial({
+        map: textureLoader.load("textures/planets/moon_1024.jpg")
+    });
+    meshMoon = new THREE.Mesh(geometry, materialMoon);
+    meshMoon.position.set(radius * 5, 0, 0);
+    meshMoon.scale.set(moonScale, moonScale, moonScale);
+    scene.add(meshMoon);
+
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
     container.appendChild( renderer.domElement );
 
     container.appendChild( stats.dom );
-    //document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-    //
-    var orbit = new THREE.OrbitControls( camera, renderer.domElement );
-    orbit.enablezoom = false;
+
+    controls = new THREE.FlyControls( camera );
+    controls.movementSpeed = 1;
+    controls.domElement = renderer.domElement;
+    controls.rollSpeed = Math.PI / 6;
+    controls.autoForward = false;
+    controls.dragToLook = true;
 
     window.addEventListener( 'resize', onWindowResize, false );
+
 }
+
 function onWindowResize() {
     windowHalfX = window.innerWidth / 2;
     windowHalfY = window.innerHeight / 2;
@@ -66,19 +114,18 @@ function onWindowResize() {
     camera.updateProjectionMatrix();
     renderer.setSize( window.innerWidth, window.innerHeight );
 }
-function onDocumentMouseMove( event ) {
-    mouseX = ( event.clientX - windowHalfX ) / 2;
-    mouseY = ( event.clientY - windowHalfY ) / 2;
-}
-//
+
 function animate() {
     requestAnimationFrame( animate );
     render();
     stats.update();
 }
+
 function render() {
-    //camera.position.x += ( mouseX - camera.position.x ) * .05;
-    //camera.position.y += ( - mouseY - camera.position.y ) * .05;
-    //camera.lookAt( scene.position );
+    var delta = clock.getDelta();
+    meshPlanet.rotation.y += rotationSpeed * delta;
+    meshClouds.rotation.y += 1.25 * rotationSpeed * delta;
+
+    controls.update( delta );
     renderer.render( scene, camera );
 }
