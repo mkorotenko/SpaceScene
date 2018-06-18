5d15115a14d3738c9dabfd0748e5ab6673c15bcd
Maxim Korotenko <m.v.korotenko@gmail.com>
2018-06-18 10:15:17 +0300

    Added Earth

--- dist/js/solarSystem.js
+++ dist/js/solarSystem.js
@@ -5,18 +5,32 @@ var create = function() {
         const nebula = modelLoader.colladaGroup('./models/collada/models/Middle_Nebula.dae')
         const solar = new Promise( resolve => (new THREE.ObjectLoader()).load('models/solarSystem.json', object => resolve(object)));
 
-    // var textureLoader = new THREE.TextureLoader();
+     var textureLoader = new THREE.TextureLoader();
     // // // planet
-    // var materialNormalMap = new THREE.MeshPhongMaterial( {
-    //      specular: 0x333333,
-    //      shininess: 15,
-    //      map: textureLoader.load( "textures/planets/earth_atmos_2048.jpg" ),
-    //      specularMap: textureLoader.load( "textures/planets/earth_specular_2048.jpg" ),
-    //      normalMap: textureLoader.load( "textures/planets/earth_normal_2048.jpg" ),
-    //      normalScale: new THREE.Vector2( 0.85, 0.85 )
-    //  } );
-    // var geometry = new THREE.IcosahedronBufferGeometry(0.2, 5);
-    // var earth = new THREE.Mesh(geometry, materialNormalMap);
+     var materialNormalMap = new THREE.MeshPhongMaterial( {
+          specular: 0x333333,
+          shininess: 15,
+          map: textureLoader.load( "textures/planets/earth_atmos_2048.jpg" ),
+          specularMap: textureLoader.load( "textures/planets/earth_specular_2048.jpg" ),
+          normalMap: textureLoader.load( "textures/planets/earth_normal_2048.jpg" ),
+          normalScale: new THREE.Vector2( 0.85, 0.85 )
+      } );
+     var geometry = new THREE.IcosahedronBufferGeometry(0.2, 5);
+     var earth = new THREE.Mesh(geometry, materialNormalMap);
+
+     var c = 24*60*60*1000
+     var l = ((Math.PI*2)/c)*30
+     var g = (Math.PI/180)*23.5
+     var earthGroup = new THREE.Group();
+     earthGroup.add(earth);
+     earthGroup.position.x = 1;
+     earthGroup.rotation.z = g;
+
+     var cnt = 0, m = Math.PI/(180*24*60*60*1000);
+        setInterval(() => { 
+        earth.rotation.y = l*cnt;
+        cnt+=100;
+    }, 30)
 
         Promise.all([nebula, solar]).then( models => {
             function getMeshes(group) {
@@ -42,7 +56,7 @@ var create = function() {
             const group = new THREE.Group();
             group.add(models[0]);
             group.add(models[1]);
-            //group.add(earth);
+            group.add(earthGroup);
 
             const light = new THREE.PointLight( 0xffffff, 1.8 );
             light.position.set(-0.00015217743389729112, 0.0013767499288792747, 0.0014427063671944287);
