81249a6aecd95a6515171b1a887e3fa352526440
Maxim Korotenko <m.v.korotenko@gmail.com>
2018-06-25 01:43:32 +0300

    Added specular

--- dist/js/shaders.js
+++ dist/js/shaders.js
@@ -1,3 +1,11 @@
+//In the vertex shader
+// "uniform mat4 modelMatrix;",
+// "uniform mat4 modelViewMatrix;",
+// "uniform mat4 projectionMatrix;",
+// "uniform mat4 viewMatrix;",
+// "uniform mat3 normalMatrix;",
+// "uniform vec3 cameraPosition;",
+//http://www.mathematik.uni-marburg.de/~thormae/lectures/graphics1/code/WebGLShaderLightMat/ShaderLightMat.html
 module.exports = {
     vs: `
     precision mediump int;
@@ -8,13 +16,25 @@ module.exports = {
     varying vec3 vPositionW;
     varying vec3 vNormalW;
 
+    varying vec3 normalInterp;
+    varying vec3 vertPos;
+
     void main(void) {
+
+        vec4 position4 = vec4(position, 1.0);
         gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
         vPositionW = vec3(modelMatrix * vec4(position, 1.0));
         vNormalW = normalize(vec3(modelMatrix * vec4(normal, 0.0)));
         vUV = uv;
+        
+        vec4 vertPos4 = modelViewMatrix * position4;
+        vertPos = vec3(vertPos4) / vertPos4.w;
+        //normalInterp = vec3(1);//vNormalW;//vec3(vec4(normalMatrix,0.0) * vec4(position, 0.0));
     }
     `,
+// In the fragment shader
+// "uniform mat4 viewMatrix;",
+// "uniform vec3 cameraPosition;",
     fs: `
     precision mediump int;
     precision mediump float;
@@ -23,6 +43,9 @@ module.exports = {
     varying vec2 vUV;
     varying vec3 vPositionW;
     varying vec3 vNormalW;
+
+    varying vec3 normalInterp;
+    varying vec3 vertPos;
     
     // Refs
     uniform vec3 lightPosition;
@@ -30,9 +53,10 @@ module.exports = {
     uniform sampler2D nightTexture;
     uniform sampler2D normalTexture;
     
+    const vec3 specColor = vec3(1.0, 1.0, 1.0);
+
     void main(void) {
-        vec3 direction = lightPosition - vPositionW;
-        vec3 lightVectorW = normalize(direction);
+        vec3 lightVectorW = normalize(lightPosition - vPositionW);
     
         vec3 normal = 2.0 * texture2D (normalTexture, vUV).rgb - 1.0;
         normal = normalize (normal);
@@ -40,12 +64,20 @@ module.exports = {
         // diffuse
         float lightDiffuse = clamp(dot(vNormalW, lightVectorW),0.015,1.0);
         float shininess = clamp(dot(normal, lightVectorW),0.01,1.0);
-    
+        float specular = 0.0;
+
+        //vec3 normal1 = normalize(normalInterp);
+        vec3 viewDir = normalize(-vertPos);
+        // this is blinn phong
+        vec3 halfDir = normalize(lightVectorW + viewDir);
+        float specAngle = max(dot(halfDir, normal), 0.0);
+        specular = pow(specAngle, 16.0);
+
         vec3 color;
         vec4 nightColor = texture2D(nightTexture, vUV).rgba;
         vec3 diffuseColor = texture2D(diffuseTexture, vUV).rgb;
     
-        color = (1.0-shininess) * diffuseColor * lightDiffuse + (nightColor.rgb * nightColor.a * pow((1.0 - lightDiffuse), 6.0));
+        color = specular * specColor + (1.0-shininess) * diffuseColor * lightDiffuse + (nightColor.rgb * nightColor.a * pow((1.0 - lightDiffuse), 6.0));
         gl_FragColor = vec4(color, 1.0);
     }
     `,
